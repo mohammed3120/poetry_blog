a8ef0aab8521f92228eb02dab3951a38f3aee67e
mohammed3120 <mohammedabduallah95@gmail.com>
2021-07-16 09:23:16 +0300

    add templates for poetries app, views, urls, navbar urls, and static files

--- poetries/urls.py
+++ poetries/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from .views import home, poetry_view, p_reflections, i_read_you_view, stories
 app_name = "poetries"
 urlpatterns = [
-    
+    path('',home, name='home'),
+
+    path('poetry/',poetry_view, name='poetry'),
+
+    path('p_reflections/',p_reflections, name='p_reflections'),
+
+    path('i_read_you/',i_read_you_view, name='i_read_you'),
+
+    path('stories/',stories, name='stories'),
 
 ]
--- poetries/views.py
+++ poetries/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context ={}
+    return render(request, 'poetries\home.html',context)
+
+def poetry_view(request):
+    context ={}
+    return render(request, 'poetries\poetry.html',context)
+
+def p_reflections(request):
+    context ={'d':'dddddddddddddd'}
+    return render(request, 'poetries\p_reflections.html',context)
+
+
+def i_read_you_view(request):
+    context ={}
+    return render(request, 'poetries\i_read_you.html',context)
+
+def stories(request):
+    context ={}
+    return render(request, 'poetries\stories.html',context)
+

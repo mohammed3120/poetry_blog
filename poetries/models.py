75cbee0c96870a590a69522fc631c8c2ca3d2379
mohammed3120 <mohammedabduallah95@gmail.com>
2021-07-16 15:58:38 +0300

    get filtered posts from views

--- poetries/models.py
+++ poetries/models.py
@@ -1,3 +1,87 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.utils import timezone
+from profiles.models import Profile
 # Create your models here.
+POST_TYPE_CHOICES = (
+    ('poetry', 'poetry'),
+    ('reflection', 'reflection'),
+    ('real story', 'real story'),
+    ('I read you', 'I read you'),
+)
+SENTIMENT_TYPE_CHOICES = (
+    ('happy','happy'),
+    ('sad','sad'),
+    ('normal','normal'),
+    ('action','action'),
+    ('romance','romance'),
+)
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=2000)
+    post_type = models.CharField(
+        max_length= 20,
+        choices=POST_TYPE_CHOICES,
+        default='poetry',
+    )
+    sentiment_type = models.CharField(
+        max_length= 20,
+        choices=SENTIMENT_TYPE_CHOICES,
+        default='normal',
+    )
+    created = models.DateTimeField(blank=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.created is None:
+            self.created = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    def get_comments(self):
+        comments = self.comment_set.all()
+        return comments
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.profile)
+    
+    def get_replies(self):
+        replies = self.reply_set.all()
+        return replies
+
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.profile)
+
+
+class Like(models.Model):
+    post = models.OneToOneField(Post,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return '{} like  {}'.format(self.profile.user.username, self.post.title)
+
--- poetries/views.py
+++ poetries/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
-
+from .models import Post,Comment,Like,Reply
 # Create your views here.
 def home(request):
     context ={}
     return render(request, 'poetries\home.html',context)
 
 def poetry_view(request):
-    context ={}
+    poetries = Post.objects.filter(post_type = 'poetry')
+    context ={'poetries':poetries}
     return render(request, 'poetries\poetry.html',context)
 
 def p_reflections(request):
-    context ={'d':'dddddddddddddd'}
+    reflections = Post.objects.filter(post_type = 'reflection')
+    context ={'reflections':reflections}
     return render(request, 'poetries\p_reflections.html',context)
 
 
-def i_read_you_view(request):
-    context ={}
-    return render(request, 'poetries\i_read_you.html',context)
-
 def stories(request):
-    context ={}
+    stories = Post.objects.filter(post_type = 'real story')
+    context ={'stories':stories}
     return render(request, 'poetries\stories.html',context)
 
+def i_read_you_view(request):
+    posts = Post.objects.filter(post_type = 'I read you')
+    context ={'posts':posts}
+    return render(request, 'poetries\i_read_you.html',context)
